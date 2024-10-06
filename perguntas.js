eeeb5218c66a74d73bcb75a6d4c132268b7e339d
Igor Felipe Leonel Luiz <137519339+leonellipe1A13@users.noreply.github.com>
2024-10-06 11:00:57 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Programção',
+    'O que é JavaScript',
+    'O JavaScript é uma linguagem de programção'
+)
+
+criaCartao(
+    'React',
+    'O que é React?',
+    'O React é uma biblioteca front-end JavaScript de código aberto com foco em criar interfaces de usuário em páginas'
+)
+
+criaCartao(
+    'Docker',
+    'O que é Docker?',
+    'Docker é um conjunto de produtos de plataforma como serviço que usam virtualização de nível de sistema operacional para entregar software em pacotes chamados contêineres.'
+)
+
+criaCartao(
+    'Next.js',
+    'O que é Next.js',
+    'Next.js é uma estrutura da web de desenvolvimento front-end React de código aberto criada por Vercel que permite funcionalidades como renderização do lado do servidor e geração de sites estáticos para aplicativos da web baseados em React'
+)
+
+criaCartao(
+    'Programção',
+    'O que é CSS',
+    'O CSS é uma linguagem de estilização'
+)
+
+criaCartao(
+    'TypeScript',
+    'O que é TypeScript',
+    'TypeScript é uma linguagem de programação de código aberto desenvolvida pela Microsoft'
+)
